2f6cb8840b8882dcc29dcddc84f0312f9313825c
Refiver <leonunruh64@gmail.com>
2025-11-06 19:42:06 +0000

    wip:first working training. but need to change dataset to follow hugging face rules (only train, test, val)

---
+++ util/jpeg_converter.py
@@ -0,0 +1,27 @@
+import os
+
+def rename_images_to_jpg_recursive(base_folder):
+    # Endungen, die vereinheitlicht werden sollen
+    extensions = [".jpeg", ".png", ".jfif", ".heic", ".webp"]
+
+    for root, _, files in os.walk(base_folder):
+        for filename in files:
+            old_path = os.path.join(root, filename)
+            name, ext = os.path.splitext(filename)
+            ext_lower = ext.lower()
+
+            if ext_lower in extensions:
+                new_name = f"{name}.jpg"
+                new_path = os.path.join(root, new_name)
+
+                # Konfliktlösung: falls Datei existiert, fortlaufende Nummer anhängen
+                counter = 1
+                while os.path.exists(new_path):
+                    new_name = f"{name}_{counter}.jpg"
+                    new_path = os.path.join(root, new_name)
+                    counter += 1
+
+                os.rename(old_path, new_path)
+                print(f"Umbenannt: {old_path} → {new_path}")
+
+rename_images_to_jpg_recursive("../data/Training Data/train")
