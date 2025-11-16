f6c3c1eaf56d94013ae29df4b18255f6235f880d
Aremju <juliusarendt@icloud.com>
2025-11-16 16:08:35 +0100

    refactor: comments and variable names

--- task/milestone_1/plot_different_animal_images.py
+++ task/milestone_1/plot_different_animal_images.py
@@ -8,7 +8,7 @@ path_to_cow_train_image = "../../data/animal_images/train/Cow/Cow-Train (5).jpeg
 path_to_hippo_train_image = "../../data/animal_images/train/Hippo/Hippo - Train (36).jpeg"
 path_to_beetle_train_image = "../../data/animal_images/train/Beetle/Beetle-Train (8).jpg"
 
-paths = [
+paths_with_label = [
     (path_to_tiger_train_image, "Tiger"),
     (path_to_elephant_train_image, "Elephant"),
     (path_to_lizard_train_image, "Lizard"),
@@ -17,11 +17,12 @@ paths = [
     (path_to_beetle_train_image, "Beetle")
 ]
 
-def plot_images_with_labels_as_grid(paths):
+def plot_images_with_labels_as_grid(paths_and_labels):
+    """Plots images with labels as a grid."""
     fig, axes = plt.subplots(2, 3, figsize=(10, 7))
-    axes = axes.ravel()  # flatten 2x3 array → 1D array of 6 axes
+    axes = axes.ravel()
 
-    for i, (path, label) in enumerate(paths):
+    for i, (path, label) in enumerate(paths_and_labels):
         if not os.path.exists(path):
             axes[i].text(0.5, 0.5, "File not found", ha="center", va="center")
             axes[i].set_title(label)
@@ -34,11 +35,8 @@ def plot_images_with_labels_as_grid(paths):
         axes[i].axis("off")
 
     plt.tight_layout()
-
-    # save plot WITHOUT white borders
     plt.savefig("different_animal_images.png", bbox_inches="tight", pad_inches=0)
-
     plt.show()
 
 if __name__ == "__main__":
-    plot_images_with_labels_as_grid(paths)
+    plot_images_with_labels_as_grid(paths_with_label)
